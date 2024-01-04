687d42b0295b6b4f86f5f7090089a24d2ff666ba
Joseph Suhudu <suhudujoseph@gmail.com>
2024-01-04 22:50:35 +0300

    use forward slash in compile.sh

--- compile.sh
+++ compile.sh
@@ -4,13 +4,13 @@ rm -rf build
 rm -rf dist
 
 # ? <<<<<<<<>>>>>>>>>>
-# cp venv\njogush-JHqgHMML-py3.8\Lib\site-packages\daphne\management\commands\runserver.py 
+# cp venv/njogush-JHqgHMML-py3.8/Lib/site-packages/daphne/management/commands/runserver.py 
 # OPTION 1 - hardcoding location of daphne
-pyinstaller myscript.py --distpath dist\linux --name njogush --icon build.png
+pyinstaller myscript.py --distpath dist/linux --name njogush --icon build.png
 # ! Stopped the embedding of daphne (see myscript.py)
-# cp venv\njogush-JHqgHMML-py3.8\Scripts\daphne.exe dist\linux\njogush
+# cp venv/njogush-JHqgHMML-py3.8/Scripts/daphne.exe dist/linux/njogush
 # ! <<<<<<<<>>>>>>>>>>
-# cp dist\linux\njogush\daphne\management\commands\runserver.py dist\linux\njogush\django\core\management\commands\runserver.py
+# cp dist/linux/njogush/daphne/management/commands/runserver.py dist/linux/njogush/django/core/management/commands/runserver.py
 
 # All these actions should probably be included in a PyInstaller hook
 # they have been however been included only because the steps are more obvious here
@@ -18,11 +18,11 @@ pyinstaller myscript.py --distpath dist\linux --name njogush --icon build.png
 # workings of PyInstaller
 # The advantage of using hooks is that files are embedded and this becomes a useful
 # feature especially when --onefile switch is used
-cp -r static dist\linux\njogush
-cp build.png dist\linux\njogush
+cp -r static dist/linux/njogush
+cp build.png dist/linux/njogush
 # TODO https://stackoverflow.com/questions/947954/how-to-have-the-cp-command-create-any-necessary-folders-for-copying-a-file-to-a/14085147#14085147
-mkdir dist\linux\njogush\build_tool\templates\build_tool_vue
-cp -r build_tool\templates\build_tool_vue\dist dist\linux\njogush\build_tool\templates\build_tool_vue\dist
+mkdir dist/linux/njogush/build_tool/templates/build_tool_vue
+cp -r build_tool/templates/build_tool_vue/dist dist/linux/njogush/build_tool/templates/build_tool_vue/dist
 
 
 
