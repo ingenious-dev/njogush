022519ff495bcf477b9e00c7fd1b1fb20cb7ae0f
Joseph Suhudu <suhudujoseph@gmail.com>
2024-01-04 21:25:24 +0300

    remove bash commands from windows compile script

--- compile.bat
+++ compile.bat
@@ -1,16 +1,15 @@
-poetry run python3 manage.py collectstatic
+poetry run python manage.py collectstatic --noinput
 
-rm -rf build
-rm -rf dist
+rmdir /s /q build
+rmdir /s /q dist
 
 @REM ? <<<<<<<<>>>>>>>>>>
-@REM cp venv\njogush-JHqgHMML-py3.8\Lib\site-packages\daphne\management\commands\runserver.py 
 @REM OPTION 1 - hardcoding location of daphne
 pyinstaller myscript.py --distpath dist\windows --name njogush --icon build.png
 @REM ! Stopped the embedding of daphne (see myscript.py)
-@REM cp venv\njogush-JHqgHMML-py3.8\Scripts\daphne.exe dist\windows\njogush
+@REM copy venv\njogush-JHqgHMML-py3.8\Scripts\daphne.exe dist\windows\njogush
 @REM ! <<<<<<<<>>>>>>>>>>
-@REM cp dist\windows\njogush\daphne\management\commands\runserver.py dist\windows\njogush\django\core\management\commands\runserver.py
+@REM copy dist\windows\njogush\daphne\management\commands\runserver.py dist\windows\njogush\django\core\management\commands\runserver.py
 
 @REM ? <<<<<<<<>>>>>>>>>>
 @REM ! SUSPENDED - Not working as expected 2023.01.01
@@ -27,7 +26,7 @@ pyinstaller myscript.py --distpath dist\windows --name njogush --icon build.png
 @REM echo File not found
 @REM )
 @REM pyinstaller myscript.py --add-binary "%p%";. --distpath dist\windows --name njogush --icon build.png
-@REM cp "%p%" dist\windows\njogush
+@REM copy "%p%" dist\windows\njogush
 @REM ! <<<<<<<<>>>>>>>>>>
 
 @REM All these actions should probably be included in a PyInstaller hook
@@ -36,13 +35,11 @@ pyinstaller myscript.py --distpath dist\windows --name njogush --icon build.png
 @REM workings of PyInstaller
 @REM The advantage of using hooks is that files are embedded and this becomes a useful
 @REM feature especially when --onefile switch is used
-cp -r static dist\windows\njogush
-cp -r static dist\windows\njogush
-cp build.png dist\windows\njogush
+xcopy static dist\windows\njogush\static /s /e /h
+copy build.png dist\windows\njogush
 @REM TODO https://stackoverflow.com/questions/947954/how-to-have-the-cp-command-create-any-necessary-folders-for-copying-a-file-to-a/14085147#14085147
 mkdir dist\windows\njogush\build_tool\templates\build_tool_vue
-cp -r build_tool\templates\build_tool_vue\dist dist\windows\njogush\build_tool\templates\build_tool_vue\dist
-
+xcopy build_tool\templates\build_tool_vue\dist dist\windows\njogush\build_tool\templates\build_tool_vue\dist /s /e /h
 
 
 @REM MORE REFERENCES
@@ -51,3 +48,5 @@ cp -r build_tool\templates\build_tool_vue\dist dist\windows\njogush\build_tool\t
 @REM https://stackoverflow.com/questions/18668947/how-do-i-set-sys-argv-so-i-can-unit-test-it
 @REM https://stackoverflow.com/questions/77123444/create-an-exe-file-from-a-django-project-with-pyinstaller
 @REM https://github.com/pyinstaller/pyinstaller/issues/2332
+@REM https://superuser.com/questions/206036/commmand-line-command-to-copy-entire-directory-including-directory-folder-to-a/206038#206038
+@REM https://stackoverflow.com/questions/97875/rm-rf-equivalent-for-windows
---
+++ compile.sh
@@ -0,0 +1,34 @@
+poetry run python manage.py collectstatic --noinput
+
+rm -rf build
+rm -rf dist
+
+# ? <<<<<<<<>>>>>>>>>>
+# cp venv\njogush-JHqgHMML-py3.8\Lib\site-packages\daphne\management\commands\runserver.py 
+# OPTION 1 - hardcoding location of daphne
+pyinstaller myscript.py --distpath dist\linux --name njogush --icon build.png
+# ! Stopped the embedding of daphne (see myscript.py)
+# cp venv\njogush-JHqgHMML-py3.8\Scripts\daphne.exe dist\linux\njogush
+# ! <<<<<<<<>>>>>>>>>>
+# cp dist\linux\njogush\daphne\management\commands\runserver.py dist\linux\njogush\django\core\management\commands\runserver.py
+
+# All these actions should probably be included in a PyInstaller hook
+# they have been however been included only because the steps are more obvious here
+# and do not require the developer to have a deeper understaning into the
+# workings of PyInstaller
+# The advantage of using hooks is that files are embedded and this becomes a useful
+# feature especially when --onefile switch is used
+cp -r static dist\linux\njogush
+cp build.png dist\linux\njogush
+# TODO https://stackoverflow.com/questions/947954/how-to-have-the-cp-command-create-any-necessary-folders-for-copying-a-file-to-a/14085147#14085147
+mkdir dist\linux\njogush\build_tool\templates\build_tool_vue
+cp -r build_tool\templates\build_tool_vue\dist dist\linux\njogush\build_tool\templates\build_tool_vue\dist
+
+
+
+# MORE REFERENCES
+# https://stackoverflow.com/questions/25794369/batch-file-wildcard-in-folderpath-no-idea-what-the-wildcard-can-be/25794897#25794897
+# https://stackoverflow.com/questions/7005951/batch-file-find-if-substring-is-in-string-not-in-a-file
+# https://stackoverflow.com/questions/18668947/how-do-i-set-sys-argv-so-i-can-unit-test-it
+# https://stackoverflow.com/questions/77123444/create-an-exe-file-from-a-django-project-with-pyinstaller
+# https://github.com/pyinstaller/pyinstaller/issues/2332
