3a61d1f6d4659d53df3f8a8fd5889ede2a361199
Joseph Suhudu <suhudujoseph@gmail.com>
2024-04-24 23:02:26 +0300

    Project Identifier, better form validation for Project

--- build_tool/templates/build_tool_vue/src/views/ProjectsView.vue
+++ build_tool/templates/build_tool_vue/src/views/ProjectsView.vue
@@ -57,7 +57,7 @@
             <div class="pointer-events-none fixed inset-y-0 right-0 flex max-w-full pl-10 sm:pl-16">
               <TransitionChild as="template" enter="transform transition ease-in-out duration-500 sm:duration-700" enter-from="translate-x-full" enter-to="translate-x-0" leave="transform transition ease-in-out duration-500 sm:duration-700" leave-from="translate-x-0" leave-to="translate-x-full">
                 <DialogPanel class="pointer-events-auto w-screen max-w-md">
-                  <form class="flex h-full flex-col divide-y divide-gray-200 bg-white shadow-xl">
+                  <form class="flex h-full flex-col divide-y divide-gray-200 bg-white shadow-xl" @submit.prevent="checkForm()">
                     <div class="h-0 flex-1 overflow-y-auto">
                       <div class="bg-indigo-700 py-6 px-4 sm:px-6">
                         <div class="flex items-center justify-between">
@@ -77,6 +77,17 @@
                         <div class="divide-y divide-gray-200 px-4 sm:px-6">
                           <div class="space-y-6 pt-6 pb-5">
                             <div>
+                              <label for="project-name" class="block text-sm font-medium text-gray-900">Project Identifier</label>
+                              <div class="mt-1">
+                                <input
+                                type="text"
+                                name="project-identifier"
+                                id="project-identifier"
+                                class="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                                v-model="identifier" required />
+                              </div>
+                            </div>
+                            <div>
                               <label for="project-name" class="block text-sm font-medium text-gray-900">Project name</label>
                               <div class="mt-1">
                                 <input
@@ -84,7 +95,7 @@
                                 name="project-name"
                                 id="project-name"
                                 class="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
-                                v-model="name" />
+                                v-model="name" required />
                               </div>
                             </div>
                             <div>
@@ -105,9 +116,9 @@
                       <div class="flex flex-shrink-0 justify-end">
                         <button type="button" class="rounded-md border border-gray-300 bg-white py-2 px-4 text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2" @click="open = false">Cancel</button>
                         <button
-                          type="button"
+                          type="submit"
                           class="ml-4 inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-                          @click="checkForm()">Save</button>
+                          >Save</button>
                       </div>
                     </div>
                   </form>
@@ -314,6 +325,7 @@ export default {
       open: false,
       selectedId: null,
       errors: [],
+      identifier: '',
       name: '',
       description: '',
       openModal: false,
@@ -363,11 +375,13 @@ export default {
       this.open = false
     },
     setForm(item) {
+      this.identifier = item.identifier;
       this.name = item.name;
       this.description = item.description;
     },
     async save() {
       const data = {
+        identifier: this.identifier,
         name: this.name,
         description: this.description,
       }
