2405bf89f6aada1c5c258dd86c53be2e17f1e352
ingenioius-dev <ingeniousdeveke@gmail.com>
2024-05-29 04:52:12 +0300

    better dynamic support for when setting BASE_URLs

--- build_tool/templates/build_tool_vue/src/network/network_info.js
+++ build_tool/templates/build_tool_vue/src/network/network_info.js
@@ -1,5 +1,6 @@
-const NJOGUSH_BASE_URL = `http://${getBaseUrl()}`;
-const NJOGUSH_WEBSOCKET_URL = `ws://${getBaseUrl()}`;
+// const NJOGUSH_BASE_URL = `http://${getBaseUrl()}`;
+const NJOGUSH_BASE_URL = `${window.location.protocol}//${getBaseUrl()}`;
+const NJOGUSH_WEBSOCKET_URL = `${window.location.protocol.includes('https') ? 'wss' : 'ws'}://${getBaseUrl()}`;
 const NJOGUSH_AUTHORITY = getBaseUrl();
 
 function getBaseUrl() {
